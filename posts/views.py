edfb831da51bd6c403a17b0bb7f24ee188d285cb
adamm-xyz <amontano495@gmail.com>
2025-07-21 14:49:57 -0700

    Added user signup flow

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
